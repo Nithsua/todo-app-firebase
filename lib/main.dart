8c3aee87a75793a2d9a44d5ed1381c02883b58c9
Nivas Muthu <nivasmuthu452@gmail.com>
2020-07-19 11:36:18 +0530

    changes to import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'todo-page.dart';
+import 'package:todo_app/pages/todo-page.dart';
 
 void main() async {
   runApp(MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:todo_app/todo-page.dart';
+import 'package:todo_app/pages/todo-page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
